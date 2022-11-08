479e7acaa9fafb222766fe1da52c15bf77616ae4
Yhossy Esteban Navas Peñaranda <yhossyenp@algartech.com>
2022-11-08 17:38:23 -0500

    Managment images products

---
+++ src/files/files.controller.ts
@@ -0,0 +1,64 @@
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseFilePipeBuilder,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { diskStorage } from 'multer';
+import { FilesService } from './files.service';
+import { fileNamer } from './helpers/fileNamer.helper';
+
+@Controller('files')
+export class FilesController {
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      //guardamos en fileSystem
+      storage: diskStorage({
+        destination: './static/uploads/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadProductImage(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /(jpg|jpeg|png|gif)$/, // Se valida la extencion del archivo
+        })
+        .addMaxSizeValidator({ maxSize: 5242880 }) // Se valida el tamaño del archivo byte
+        .build({
+          errorHttpStatusCode: HttpStatus.BAD_REQUEST,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${
+      file.filename
+    }`;
+
+    return { secureUrl };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const log = new Logger(bootstrap.name);
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
@@ -13,6 +14,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+  log.log(`App Running on PORT ${process.env.PORT}`);
 }
 bootstrap();
